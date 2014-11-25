dcd210cf805696c1ecb4a6fc3a929ae270f4bc80
joelhoward0 <joelhoward0@gmail.com>
2014-11-25 21:30:31 +0000

    adds jQuery and an app.js file, rudimentary asset management in app.js

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var express = require('express');
-var app = express();
+    app = express(),
+    fs = require('fs-extra');
 
+app.use(express.static(__dirname + '/public/tmp/js'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
    res.render('index.ejs', { name: 'Shoopda' });
+});
+
+fs.remove(__dirname + '/public/tmp/', function() {
+  fs.copy(__dirname + '/public/js/app.js', __dirname + '/public/tmp/js/app.js', function() {    
+    fs.copy(__dirname + '/public/js/vendor', __dirname + '/public/tmp/js/vendor', function() {    
+      console.log('done');
+    });
+  });
 });
 
 var server = app.listen(process.env.PORT, function() {
