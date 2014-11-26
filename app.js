55fa1d0e399f7fde2d246b54170e0f8ea7a0ffcd
joelhoward0 <joelhoward0@gmail.com>
2014-11-26 01:06:21 +0000

    uses express.io to wire up sockets with sessions

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-var express = require('express'),
+var express = require('express.io'),
     app = express(),
     util = require('./util/util.js');
+app.http().io();
 
 app.use(express.static(__dirname + '/public/tmp'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
+app.use(express.cookieParser());
+app.use(express.session({secret: 'never tell aynone this deathly surprise'}));
 
 app.get('/', function (req, res) {
    res.render('index.ejs', { name: 'Shoopda' });
+});
+
+app.io.route('ready', function(req) {
+  req.io.emit('message', 'i have sending u message'); 
 });
 
 util.bundle().then(function() { console.log('all done bundling'); });
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,10 @@
 $(function() {
-  console.log('starting up, yo');
+  window.app = {};
+  var app = window.app;
+  
+  app.io = window.io.connect();
+  app.io.emit('ready');
+  app.io.on('message', function(data) {
+    alert('message: ' + data);
+  });
 });
